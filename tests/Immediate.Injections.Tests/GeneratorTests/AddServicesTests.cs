cc595180b78d674aabe84a8c83772133fcc202d8
Stuart Turner <stuart@turner-isageek.com>
2026-07-10 19:42:22 -0500

    Add generated output test for `AddXxxServices` by tfm

---
+++ tests/Immediate.Injections.Tests/GeneratorTests/AddServicesTests.cs
@@ -0,0 +1,29 @@
+namespace Immediate.Injections.Tests.GeneratorTests;
+
+public sealed class AddServicesTests
+{
+	[Theory]
+	[MemberData(nameof(Frameworks))]
+	public async Task ValidAddServicesMethod(string framework)
+	{
+		var result = GeneratorTestHelper.RunGenerator(
+			"""
+			using Immediate.Injections.Shared;
+			using Microsoft.Extensions.DependencyInjection;
+			"""
+		);
+
+		Assert.Equal(
+			[
+				"Immediate.Injections.Generators/Immediate.Injections.Generators.ImmediateInjectionsGenerator/II.ServiceCollectionExtensions.g.cs",
+			],
+			result.GeneratedTrees.Select(t => t.FilePath.Replace('\\', '/'))
+		);
+
+		_ = await Verify(result)
+			.UseParameters(framework);
+	}
+
+	public static TheoryData<string> Frameworks =>
+		[Utility.ReferenceAssemblies.TargetFramework];
+}

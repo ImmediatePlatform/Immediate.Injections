bd00eb18a347d1a77faee83c2483b8d3529b6c5d
Stuart Turner <stuart@turner-isageek.com>
2026-07-27 18:31:57 -0500

    Address BDN use of `-`

--- src/Immediate.Injections.Generators/ImmediateInjectionsGenerator.cs
+++ src/Immediate.Injections.Generators/ImmediateInjectionsGenerator.cs
@@ -162,6 +162,7 @@ file static class Extensions
 		return (compilation.AssemblyName ?? string.Empty)
 			.Replace(".", string.Empty, StringComparison.Ordinal)
 			.Replace(" ", string.Empty, StringComparison.Ordinal)
+			.Replace("-", string.Empty, StringComparison.Ordinal)
 			.Trim();
 	}
 }
